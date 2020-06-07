d0e09bd29457aaa2b5c47067c119a732fdf519cd
YeisonTapia <yftapial@ut.edu.co>
2020-06-07 11:44:42 -0500

    Update 404 page

--- src/pages/NotFount.js
+++ src/pages/NotFount.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const NotFount = () => (
-  <h1>No Encontrado</h1>
+  <div className="NotFound">
+    <div className="NotFound-container">
+      <div className="NotFound-content">
+        <h3>Ups... Algo salio mal...</h3>
+      </div>
+    </div>
+  </div>
 );
 
 export default NotFount;
